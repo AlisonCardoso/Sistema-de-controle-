e1f000390ffa7aec4aba51b7a27408ea7c8be5c0
AlisonCardoso <88635010+AlisonCardoso@users.noreply.github.com>
2024-07-11 10:40:06 -0300

    vomando regional

---
+++ cadastro-veiculos/app/Http/Controllers/SubCommandController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubCommand;
+use Illuminate\Http\Request;
+
+class SubCommandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubCommand $subCommand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubCommand $subCommand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SubCommand $subCommand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubCommand $subCommand)
+    {
+        //
+    }
+}
---
+++ cadastro-veiculos/app/Models/RegionalCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RegionalCommand extends Model
+{
+    use HasFactory;
+}
